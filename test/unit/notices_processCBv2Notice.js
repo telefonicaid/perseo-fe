d65048c1dffcf82bf2dcee647d7b1e09960eab4d
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2021-10-26 11:06:22 +0200

    fix value

--- test/unit/notices_processCBv2Notice.js
+++ test/unit/notices_processCBv2Notice.js
@@ -404,7 +404,7 @@ describe('Notices NGSIv2', function() {
                 expect(noticeResult[attrKey + '__metadata__' + at + '__lon']).to.equal(long);
                 expect(noticeResult[attrKey + '__metadata__' + at + '__x']).to.equal(x);
                 expect(noticeResult[attrKey + '__metadata__' + at + '__y']).to.equal(y);
-                parseLocationMock.should.have.been.calledWith(locValue2);
+                parseLocationMock.should.have.been.calledWith(locValue2.coordinates.reverse().toString());
                 parseLocationMock.should.be.calledOnce;
                 done();
             });
