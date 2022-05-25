ba47169782febb3ad57c6a3e3bf5d72449070317
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2022-05-25 10:04:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -228,7 +228,7 @@ config.restBase = null;
 config.castTypes = false;
 
 /**
- * Maximun number of rules triggered by the some notification (correlator) until refuse execute a rule
+ * Maximum number of rules triggered by the same notification (with the same correlator) until refuse execute a rule
  */
 config.maxRulesByCorr = 20;
 
