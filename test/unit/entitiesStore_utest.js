f71575e3607ccd74944d4d2a4a03942c02884d9b
RafaelM3 <rmunoz@mb3-gestion.com>
2023-06-26 12:27:15 +0200

    findSilentEntitiesByAPI test

--- test/unit/entitiesStore_utest.js
+++ test/unit/entitiesStore_utest.js
@@ -28,59 +28,99 @@
 var should = require('should');
 var rewire = require('rewire');
 var entitiesStore = rewire('../../lib/models/entitiesStore.js');
-//var assert = require('assert');
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
+var expect = chai.expect;
 var config = require('../../config.js');
+var NGSI = require('ngsijs');
 chai.Should();
 chai.use(sinonChai);
 
-describe('Test suit', function() {
-    var ruleData = {
-            name: 'NSR2',
-            action: {
-                type: 'update',
-                parameters: {
-                    id: 'alarma:${id}',
-                    type: 'Alarm',
-                    attributes: [
-                        {
-                            name: 'msg',
-                            value: 'El status de ${id} es ${status}'
-                        }
-                    ]
+describe('entitiesStore', function() {
+    describe('#findSilentEntitiesByAPI', function() {
+        var connectionMock;
+        var listEntitiesMock;
+
+        beforeEach(function() {
+            connectionMock = sinon.stub(NGSI, 'Connection');
+            listEntitiesMock = sinon.stub();
+
+            // Mock the Connection function
+            connectionMock.returns({ v2: { listEntities: listEntitiesMock } });
+        });
+
+        afterEach(function() {
+            // Restore the original function after each test
+            connectionMock.restore();
+        });
+
+        var ruleData = {
+                name: 'NSR2',
+                action: {
+                    type: 'update',
+                    parameters: {
+                        id: 'alarma:${id}',
+                        type: 'Alarm',
+                        attributes: [
+                            {
+                                name: 'msg',
+                                value: 'El status de ${id} es ${status}'
+                            }
+                        ]
+                    }
+                },
+                subservice: '/',
+                service: 'unknownt',
+                nosignal: {
+                    checkInterval: '1',
+                    attribute: 'temperature',
+                    reportInterval: '5',
+                    id: 'thing:disp1',
+                    idRegexp: null,
+                    type: 'thing'
                 }
             },
-            subservice: '/',
-            service: 'unknownt',
-            nosignal: {
-                checkInterval: '1',
-                attribute: 'temperature',
-                reportInterval: '5',
-                id: 'thing:disp1',
-                idRegexp: null,
-                type: 'thing'
-            }
-        },
-        func = 'sinon.stub()',
-        callback = function(e, request) {
-            should.exist(request);
-            should.not.exist(e);
-            should.equal(request.httpCode, 200);
-        };
+            func = 'sinon.stub()',
+            callback = function(e, request) {
+                should.exist(request);
+                should.not.exist(e);
+                should.equal(request.httpCode, 200);
+            };
 
-    it('By default should call findSilentEntitiesByMongo', function() {
-        var findSilentEntitiesByMongoSpy = sinon.spy();
-        entitiesStore.__set__('findSilentEntitiesByMongo', findSilentEntitiesByMongoSpy);
-        entitiesStore.FindSilentEntities(ruleData.service, ruleData.subservice, ruleData, func, callback);
-        sinon.assert.calledOnce(findSilentEntitiesByMongoSpy);
-    });
-    it('If default settings are changed FindSilentEntitiesByAPI should be called', function() {
-        config.nonSignalByAPI = true;
-        var findSilentEntitiesByAPISpy = sinon.spy();
-        entitiesStore.__set__('findSilentEntitiesByAPI', findSilentEntitiesByAPISpy);
-        entitiesStore.FindSilentEntities();
-        sinon.assert.calledOnce(findSilentEntitiesByAPISpy);
+        it('By default should call findSilentEntitiesByMongo', function() {
+            var findSilentEntitiesByMongoSpy = sinon.spy();
+            entitiesStore.__set__('findSilentEntitiesByMongo', findSilentEntitiesByMongoSpy);
+            entitiesStore.FindSilentEntities(ruleData.service, ruleData.subservice, ruleData, func, callback);
+            sinon.assert.calledOnce(findSilentEntitiesByMongoSpy);
+        });
+
+        it('If default settings are changed FindSilentEntitiesByAPI should be called', function() {
+            config.nonSignalByAPI = true;
+            var findSilentEntitiesByAPISpy = sinon.spy();
+            entitiesStore.__set__('findSilentEntitiesByAPI', findSilentEntitiesByAPISpy);
+            entitiesStore.FindSilentEntities();
+            sinon.assert.calledOnce(findSilentEntitiesByAPISpy);
+        });
+
+        it('should return silent entities', async function() {
+            var funcM = sinon.spy(),
+                callbackM = sinon.spy();
+
+            // Mock the listEntities function to resolve with a test response
+            listEntitiesMock.returns(Promise.resolve({ results: [] }));
+
+            await entitiesStore.findSilentEntitiesByAPI(
+                ruleData.service,
+                ruleData.subservice,
+                ruleData,
+                funcM,
+                callbackM
+            );
+
+            expect(listEntitiesMock.calledOnce).to.be.true;
+            expect(funcM.callCount).to.equal(0);
+            expect(callbackM.calledOnceWith(null, [])).to.be.true;
+        });
     });
 });
