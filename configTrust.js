af7736f53b419b113a219d019a1afe937d96226c
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2023-09-25 10:11:48 +0200

    Create configTrust.js

---
+++ configTrust.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * List of pre-configured trusts
+ */
+var configTrust = {};
+
+configTrust.trusts = [
+    {
+        id: 'trust1',
+        user: 'user1',
+        password: 'password',
+        service: 'domain1'
+    },
+    {
+        id: 'trust2',
+        user: 'user2',
+        password: 'password2',
+        service: 'domain2'
+    }
+];
